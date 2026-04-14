2c8ee08afcd79982eb70d5bf9612544768f95054
benji <karasik.benjamin28@gmail.com>
2026-04-13 22:19:02 -0400

    feat(ux): add validation + controller and fetch by id, create poll

--- backend/poll/src/main/java/com/poll/poll/repository/PollOptionRepository.java
+++ backend/poll/src/main/java/com/poll/poll/repository/PollOptionRepository.java
@@ -3,6 +3,9 @@ package com.poll.poll.repository;
 import com.poll.poll.entity.PollOption;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PollOptionRepository extends JpaRepository<PollOption, Long> {
 
+    List<PollOption> findAllByPollId(Long id);
 }
--- backend/poll/src/main/java/com/poll/poll/repository/PollRepository.java
+++ backend/poll/src/main/java/com/poll/poll/repository/PollRepository.java
@@ -4,4 +4,5 @@ import com.poll.poll.entity.Poll;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PollRepository extends JpaRepository<Poll, Long> {
+
 }
